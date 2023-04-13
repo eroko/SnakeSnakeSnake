2e1bc7f6d5c4d1ef43d2e53e595d7bdaf86c384c
Eroko <cloverqi@gmail.com>
2023-04-13 15:56:06 +0800

    Complete Food Class

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,34 @@
 // import styles
 import "./style/index.less";
 
-console.log(123);
+class Food {
+  // 定义元素所对应的元素
+  element: HTMLElement;
+
+  constructor() {
+    // 获取页面内的food元素，赋值给element
+    this.element = document.getElementById("food")!;
+  }
+
+  // get food position
+  get x() {
+    return this.element.offsetLeft;
+  }
+
+  get y() {
+    return this.element.offsetTop;
+  }
+
+  // change food position
+  // 0~290
+  // snake move 10px per step, food pos%10=0
+
+  change() {
+    this.element.style.left = Math.round(Math.random() * 29) * 10 + "px";
+    this.element.style.top = Math.round(Math.random() * 29) * 10 + "px";
+  }
+}
+
+const food = new Food();
+food.change();
+console.log(food.x, food.y);
